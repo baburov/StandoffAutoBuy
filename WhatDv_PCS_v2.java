ef19d83bf79dc7a8574c955fe09dd32c73421965
baburov <125465617+baburov@users.noreply.github.com>
2024-01-25 12:34:11 +0300

    add v2

---
+++ WhatDv_PCS_v2.java
@@ -0,0 +1,62 @@
+
+int stickCount = 3;
+int stickSize = 52;
+
+
+var stickL = Point.get(1479,581);
+var stickR = Point.get(1479 + stickCount * stickSize,602); 
+
+var buy = Point.get(2240,569);
+
+
+var confirm = Point.get(1197,748);
+
+var upd = Point.get(1449,459);
+
+int slotH = 144;
+
+int slotA = 3;
+
+int updColor = 6381138;
+
+startScreenCapture(2);
+
+sleep(5);
+
+long kdtime = Time.getMillis();
+
+
+long slpS = 1000;
+
+for (;;)
+{ 
+     for(int q = 0; q < slotA; q++)
+     {
+          Point lp = Point.get();    
+          Point rp = Point.get();
+          lp.x = stickL.x;
+          lp.y = stickL.y + (q * slotH);
+          rp.x = stickR.x;
+          rp.y = stickR.y + (q * slotH);
+            
+          if(getContoursCount (lp, rp) > 2 && getColor(buy.x, buy.y + (slotH * q)) < 11077777)
+          {
+               click(buy.x, buy.y + (slotH * q));                                     
+               sleep(25);
+               clickRand(confirm, 0);
+               sleep(120);
+               clickRand(confirm, 0);
+               sleep(100);
+               log("buy › "+Time.getTime());
+          }
+          if ((((Time.getMillis() - kdtime) / slpS) % 2 == 0) && getColor(buy.x, buy.y + (slotH * q)) > 14000000)
+          {
+               clickRand(upd, 0);
+               sleep(100);
+               clickRand(upd, 0);
+               sleep(300)
+
+               break;
+          }
+     }
+}
