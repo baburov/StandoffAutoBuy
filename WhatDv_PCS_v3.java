caca89db902f21e979948028c855b41ff4b5fc7a
baburov <125465617+baburov@users.noreply.github.com>
2024-01-25 16:51:31 +0300

    Update WhatDv_PCS_v3.java

--- WhatDv_PCS_v3.java
+++ WhatDv_PCS_v3.java
@@ -11,7 +11,6 @@ int errorColor = 9992742;
 
 var stickL = Point.get(1475,581);
 var stickR = Point.get(1475 + (5 - stickCount) * stickSize,602); 
-var firstStickR = Point.get(1475 + 4 * stickSize,602); 
 
 var buy = Point.get(2240,569);
 
@@ -35,9 +34,9 @@ int itt = 0;
 
 Point lp = Point.get();    
 Point rp = Point.get();
-Point fr = Point.get();
 Point priceLItt = Point.get();    
 Point priceRItt = Point.get();   
+long secItter = 0;
 for (;;)
 { 
     for(int q = 0; q < slotA; q++)
@@ -46,17 +45,16 @@ for (;;)
         lp.y = stickL.y + (q * slotH);
         rp.x = stickR.x;
         rp.y = stickR.y + (q * slotH);
-        fr.y = firstStickR.y + (q * slotH);
         priceLItt.x = priceL.x;
         priceLItt.y = priceL.y + (q * slotH);
         priceRItt.x = priceR.x;
         priceRItt.y = priceR.y + (q * slotH);
-        if (getContoursCount (lp, rp) > 1 && getColor(buy.x, buy.y + (slotH * q)) < 11077777 || hasImage(selected_price, priceLItt, priceRItt) && getContoursCount (lp, fr) > 1)
+        if (getContoursCount (lp, rp) > 1 && getColor(buy.x, buy.y + (slotH * q)) < 11077777 || hasImage(selected_price, priceLItt, priceRItt))
         {
             click(buy.x, buy.y + (slotH * q));  
             sleep(25);
             clickRand(confirm, 0);
-            sleep(150);
+            sleep(350);
             if (getColor(error.x, error.y) <= errorColor + 1000)
             {
                 clickRand(confirm, 0);
@@ -66,8 +64,9 @@ for (;;)
             else
                 log("buy › "+Time.getTime());
         }
-        if ((((Time.getMillis() - kdtime) / slpS) % 10 == (itt++ % 10)) && getColor(buy.x, buy.y + (slotH * q)) > 14000000)
+        if ((((Time.getMillis() - kdtime) / slpS) != secItter) && getColor(buy.x, buy.y + (slotH * q)) > 14000000)
         {
+            secItter = ((Time.getMillis() - kdtime) / slpS);
             sleep(100);
             clickRand(upd, 0);
             sleep(100);
