ec35c837a060f0764fc32345fd12f1e1dbf106dc
baburov <125465617+baburov@users.noreply.github.com>
2024-01-29 21:13:34 +0300

    Update WhatDv_PCS_speed_price.java

--- WhatDv_PCS_speed_price.java
+++ WhatDv_PCS_speed_price.java
@@ -1,8 +1,10 @@
 Image selected_price_1 = Image.load("zero_with_point");
 Image selected_price_2 = Image.load("one_with_zero");
 
-var priceL = Point.get(1750, 547);
+var priceL = Point.get(1750, 562);
 var priceR = Point.get(1844, 605);
+var priceL2 = Point.get(1750, 562 + 144);
+var priceR2 = Point.get(1844, 605 + 144);
 
 var viewExit = Point.get(304, 951);
 
@@ -25,44 +27,56 @@ startScreenCapture(2);
 setMinMatchQuality(97); 
 sleep(5);
 long kdtime = Time.getMillis();
-long slpS = 1000;
+long bufTime = 0;
+long slpS = 1500;
 long secItter = 0;
+int updErrorFlag = 0;
 for (;;)
 { 
-        if (hasImg(selected_price_1, priceL, priceR) || hasImg(selected_price_2, priceL, priceR))
+        if (hasImg(selected_price_1, priceL, priceR) && getColor(buy.x, buy.y) < 11077777)
         {
-            click(buy.x, buy.y);  
-            sleep(25);
-            clickRand(confirm, 0);
-            sleep(300);
-            log("try to buy › "+Time.getTime());
-            click(viewExit.x, viewExit.y); 
-            sleep(50);
+                click(buy.x, buy.y);  
+                sleep(15);
+                clickRand(confirm, 0);
+                sleep(500);
+                log("try buy 1› "+Time.getTime());
+                click(viewExit.x, viewExit.y); 
+                sleep(50);
         }
-        if (getColor(error.x, error.y) <= errorColor + 1000)
+        if (hasImg(selected_price_1, priceL2, priceR2) && getColor(buy.x, buy.y + 144) < 11077777)
         {
-            clickRand(confirm, 0);
-            sleep(120);
-            log("error buy › "+Time.getTime());
-            click(viewExit.x, viewExit.y); 
-            sleep(50);
+                click(buy.x, buy.y + 144);  
+                sleep(15);
+                clickRand(confirm, 0);
+                sleep(500);
+                log("try buy 2› "+Time.getTime());
+                click(viewExit.x, viewExit.y); 
+                sleep(50);
         }
-        if ((((Time.getMillis() - kdtime) / slpS) != secItter) && getColor(buy.x, buy.y) > 14000000)
+        if (getColor(error.x, error.y) < 11077777)
         {
-            secItter = ((Time.getMillis() - kdtime) / slpS);
-            sleep(100);
-            clickRand(upd, 0);
-            sleep(100);
-            clickRand(upd, 0);
-            sleep(250);
-            click(viewExit.x, viewExit.y); 
-            sleep(50);
+                clickRand(confirm, 0);
+                sleep(50);
+                log("error buy 1› "+Time.getTime());
+                click(viewExit.x, viewExit.y); 
+                sleep(50);
         }
-        if (getColor(upd.x, upd.y) < 10000000)
+        bufTime = (Time.getMillis() - kdtime) / slpS;
+        if (bufTime != secItter && (getColor(buy.x, buy.y) > 14000000 || bufTime % 8 == 0))
         {
-            sleep(100);
+            secItter = bufTime;
+            sleep(50);
             clickRand(upd, 0);
             sleep(50);
+            clickRand(upd, 0);
+            sleep(50);
+            if (getColor(upd.x, upd.y) < 10000000)
+            {
+                    secItter = bufTime;
+                    sleep(50);
+                    clickRand(upd, 0);
+                    log("upd bug › "+Time.getTime());
+            }
             click(viewExit.x, viewExit.y); 
             sleep(50);
         }
